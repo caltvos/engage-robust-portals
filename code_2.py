decb58f4e7a15a9f53b970e3e1dcb4758bbf0bfb
caltvos <152297554+caltvos@users.noreply.github.com>
2023-12-13 06:09:34 +0000

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,58 @@
+    }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+}
+        for (int i = 0; i < 10; i++) {
+
+        }    });
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    return data;
+main();
+    }
+}
+
+    data.forEach(item => {
+
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+function generateRandomData() {
+    const data = generateRandomData();
+function main() {
+    data.forEach(item => {
+    });
+    const data = [];
+
+        console.log("Random Number: " + item);
+
+}
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    for (let i = 0; i < 10; i++) {
+}
+    return data;
+main();
+    return data
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def main():
+
+    main()
+if __name__ == "__main__":
+import random
+def generate_random_data():
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
